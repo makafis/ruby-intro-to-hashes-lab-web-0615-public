03f698241a233360d8f46bfba6c0fb5b466135ce
makafis <vinnyalfieri@gmail.com>
2015-06-05 13:24:32 -0400

    complete

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -4,8 +4,8 @@ describe "working with hashes" do
     it "creates an empty hash and assigns it to a variable called getting_started" do
       # create your hash here
       # replace the underscore in expect with your hash variable name
-
-      expect(__).to be_a(Hash)
+      new_hash = Hash.new
+      expect(new_hash).to be_a(Hash)
     end
   end
 
@@ -13,10 +13,10 @@ describe "working with hashes" do
     it "creates a hash named instructor whose key is a symbol name and value is a string Katie" do
       # create your hash here
       # replace the underscore in expect with the name of your hash
-
-      expect(__).to be_a(Hash)
-      expect(__.keys[0].to_s).to eq("name")
-      expect(__.values[0]).to eq("Katie")
+      instructor ={:name => "Katie"}
+      expect(instructor).to be_a(Hash)
+      expect(instructor.keys[0].to_s).to eq("name")
+      expect(instructor.values[0]).to eq("Katie")
     end
   end
 end
@@ -30,7 +30,15 @@ describe "building a multidimensional monopoly hash" do
       # make sure your solution is programmatic; meaning that the below is incorrect even though the test will pass
       # monopoly = {:rails => {} }
       # if you need help, read the section on Common Uses¶ in http://ruby-doc.org/core-2.1.3/Hash.html
+      monopoly = {
+          :railroads =>
+          {
 
+
+          }
+
+
+      }
       expect(monopoly.keys.count).to eq(1)
       expect(monopoly.values).to match_array([{}])
     end
@@ -43,6 +51,17 @@ describe "building a multidimensional monopoly hash" do
       # hence the solution to this (and the rest of the tests in this lab) builds on top of each other.
       # You might find it helpful to rewrite the solution to the test above here
       # before adding the solution to this test
+       monopoly = {
+          :railroads =>
+          {
+            :pieces => 4
+            
+          }
+
+
+      }
+
+
 
       expect(monopoly.values[0].values[0]).to eq(4)
       expect(monopoly.values[0].keys[0]).to eq(:pieces)
@@ -50,11 +69,42 @@ describe "building a multidimensional monopoly hash" do
 
     it "sets the second key of railroads hash to a symbol names whose value is an empty hash" do
 
+      monopoly = {
+          :railroads =>
+          {
+            :pieces => 4,
+            :names => {
+
+              }
+          }
+
+
+      }
+
       expect(monopoly.values[0].values).to eq([4, {}])
       expect(monopoly.values[0].keys).to eq([:pieces, :names])
     end
 
     it "sets the third key of railroads hash to a string rent_in_dollars whose value is an empty hash " do
+              monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                    :names => {
+
+                    }, #end names hash,
+                    "rent_in_dollars" => {
+
+                    }#end rent_in_dollars hash
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+
+
 
       expect(monopoly.values[0].values).to eq([4, {}, {}])
       expect(monopoly.values[0].keys).to eq([:pieces, :names, "rent_in_dollars"])
@@ -68,28 +118,106 @@ describe "building a multidimensional monopoly hash" do
       # You might find it helpful to rewrite the solution to the test above here
       # before adding the solution to this test
 
+
+
       it "sets the 1st key of rent_in_dollars hash to a symbol one_piece_owned whose value is the integer 25" do
 
+                           monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+
+                    },#end rent_in_dollars hash
+                    :names => {
+
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+        monopoly[:railroads]["rent_in_dollars"][:one_piece_owned] = 25
         expect(monopoly.values[0].values.count).to eq(3)
         expect(monopoly.values[0]["rent_in_dollars"].values[0]).to eq(25)
         expect(monopoly.values[0]["rent_in_dollars"].keys[0]).to eq(:one_piece_owned)
       end
 
       it "sets the 2nd key of rent_in_dollars hash to a symbol two_pieces_owned whose value is the integer 50" do
+                                   monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25
+                    },#end rent_in_dollars hash
+                    :names => {
 
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+               monopoly[:railroads]["rent_in_dollars"][:two_pieces_owned] = 50
         expect(monopoly.keys.count).to eq(1)
         expect(monopoly.values[0].keys.count).to eq(3)
         expect(monopoly.values[0]["rent_in_dollars"][:two_pieces_owned]).to eq(50)
       end
 
       it "sets the 3rd key of rent_in_dollars hash to a symbol three_pieces_owned whose value is the integer 100" do
+                   monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50
+                    },#end rent_in_dollars hash
+                    :names => {
 
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+
+              monopoly[:railroads]["rent_in_dollars"][:three_pieces_owned] = 100
         expect(monopoly.values[0].values.count).to eq(3)
         expect(monopoly.values[0]["rent_in_dollars"].keys[0]).to eq(:one_piece_owned)
         expect(monopoly.values[0]["rent_in_dollars"][:three_pieces_owned]).to eq(100)
       end
 
       it "sets the 4th key of rent_in_dollars hash to a symbol four_pieces_owned whose value is the integer 200" do
+                   monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100
+                    },#end rent_in_dollars hash
+                    :names => {
+
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+              monopoly[:railroads]["rent_in_dollars"][:four_pieces_owned] = 200
 
         expect(monopoly.values[0].values.count).to eq(3)
         expect(monopoly.values[0]["rent_in_dollars"].keys.count).to eq(4)
@@ -103,12 +231,62 @@ describe "building a multidimensional monopoly hash" do
     context "adding a hash-value to the third level of the hash" do #nesting name will lead to fourth level nesting
       it "sets the 1st key of names to a symbol reading_railroad whose value is an empty hash" do
 
+                   monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+                    :names => {
+
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+              monopoly[:railroads][:names][:reading_railroad] = {}
         expect(monopoly.keys.count).to eq(1)
         expect(monopoly.values[0].values.count).to eq(3)
         expect(monopoly.values[0][:names].values[0]).to eq({})
       end
 
       it "sets the 2nd key of names hash to a symbol pennsylvania_railroad whose value is an empty hash" do
+
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+
+                          }#end reading_railroad
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+              monopoly[:railroads][:names][:pennsylvania_railroad] = {}
+
+
 
         expect(monopoly.keys.count).to eq(1)
         expect(monopoly.values[0].values.count).to eq(3)
@@ -117,12 +295,73 @@ describe "building a multidimensional monopoly hash" do
 
       it "sets the 3rd key of names hash to a symbol b_and_o_railroad whose value is an empty hash" do
 
+
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+
+                          },#end reading_railroad
+                          :pennsylvania_railroad => {
+
+                          }#end penn railroad
+
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+              monopoly[:railroads][:names][:b_and_o_railroad] = {}
+
         expect(monopoly.keys.count).to eq(1)
         expect(monopoly.values[0].values.count).to eq(3)
         expect(monopoly.values[0][:names].values[2]).to eq({})
       end
 
       it "sets the 4th key of names hash to a symbol shortline whose value is an empty hash" do
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+
+                          },#end reading_railroad
+                          :pennsylvania_railroad => {
+
+                          },#end penn railroad
+                          :b_and_o_railroad => {}
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+              monopoly[:railroads][:names][:shortline] = {}
+
 
         expect(monopoly.keys.count).to eq(1)
         expect(monopoly.values[0].values.count).to eq(3)
@@ -132,12 +371,83 @@ describe "building a multidimensional monopoly hash" do
       context "adding key-value pairs to the fourth-level of the hash" do
         it "sets the 1st key of reading_railroad hash to a string mortgage_value whose value is a string $100" do
 
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+
+                          },#end reading_railroad
+                          :pennsylvania_railroad => {
+
+                          },#end penn railroad
+                          :b_and_o_railroad => {
+
+                          },#end b_and_o
+                          :shortline => {
+
+                          } #end shortline
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+monopoly[:railroads][:names][:reading_railroad]["mortgage_value"] = "$100"
+
           expect(monopoly.values[0][:names].has_key?(:reading_railroad)).to be true
           expect(monopoly.values[0][:names].values[0].has_key?("mortgage_value")).to be true
           expect(monopoly.values[0][:names].values[0].values[0]).to eq("$100")
         end
 
         it "sets the 1st key of pennsylvania_railroad hash to a string mortgage_value whose value is a string $200" do
+
+
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+                            "mortgage_value" => "$100"
+                          },#end reading_railroad
+                          :pennsylvania_railroad => {
+
+                          },#end penn railroad
+                          :b_and_o_railroad => {
+
+                          },#end b_and_o
+                          :shortline => {
+
+                          } #end shortline
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+                monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
 
           expect(monopoly.values[0][:names].has_key?(:pennsylvania_railroad)).to be true
           expect(monopoly.values[0][:names].values[1].has_key?("mortgage_value")).to be true
@@ -146,12 +456,86 @@ describe "building a multidimensional monopoly hash" do
 
         it "sets the 1st key of b_and_o_railroad hash to a string mortgage_value whose value is a string $400" do
 
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+                            "mortgage_value" => "$100"
+                          },#end reading_railroad
+                          :pennsylvania_railroad => {
+                            "mortgage_value" => "$200"
+                          },#end penn railroad
+                          :b_and_o_railroad => {
+
+                          },#end b_and_o
+                          :shortline => {
+
+                          } #end shortline
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+                monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+
+
+
+
           expect(monopoly.values[0][:names].has_key?(:b_and_o_railroad)).to be true
           expect(monopoly.values[0][:names].values[2].has_key?("mortgage_value")).to be true
           expect(monopoly.values[0][:names].values[2].values[0]).to eq("$400")
         end
 
         it "sets the 1st key of shortline hash to a string mortgage_value whose value is a string $800" do
+               monopoly = {
+                  :railroads =>
+                  {
+                    :pieces => 4,
+                     "rent_in_dollars" => {
+                      :one_piece_owned => 25,
+                      :two_pieces_owned => 50,
+                      :three_pieces_owned => 100,
+                      :four_pieces_owned => 200
+                    },#end rent_in_dollars hash
+
+                    :names => {
+                          :reading_railroad => {
+                            "mortgage_value" => "$100"
+                          },#end reading_railroad
+                          :pennsylvania_railroad => {
+                            "mortgage_value" => "$200"
+                          },#end penn railroad
+                          :b_and_o_railroad => {
+                            "mortgage_value" => "$400"
+                          },#end b_and_o
+                          :shortline => {
+
+                          } #end shortline
+                    } #end names hash,
+
+
+
+                  } #end railroads hash
+
+
+              } #end monopoly hash
+
+                monopoly[:railroads][:names][:shortline]["mortgage_value"]= "$800"
+
+
 
           expect(monopoly.values[0][:names].keys[3]).to eq(:shortline)
           expect(monopoly.values[0][:names].values[3].has_key?("mortgage_value")).to be true
